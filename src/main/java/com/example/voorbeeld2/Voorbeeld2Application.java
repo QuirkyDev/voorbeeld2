ab07f67e752a86883842fe7e83b7a72bbee7dcb1
= <=>
2020-10-19 16:20:29 +0200

    init project

---
+++ src/main/java/com/example/voorbeeld2/Voorbeeld2Application.java
@@ -0,0 +1,13 @@
+package com.example.voorbeeld2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Voorbeeld2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Voorbeeld2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/voorbeeld2/Voorbeeld2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voorbeeld2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Voorbeeld2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
